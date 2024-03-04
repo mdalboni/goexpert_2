56a292219f1cdae8a9fabb65f1c467ab9b10d355
MDalboni <maxwell@dalboni.land>
2024-03-04 08:37:48 -0300

    Updating requests to run until context expires.

--- cmd/main.go
+++ cmd/main.go
@@ -12,29 +12,9 @@ func main() {
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
-	go func() {
-		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-		defer cancel()
-		service := services.NewBrazilApiZipCodeService()
-		result, url, err := service.GetZipCode(ctx, "01153000")
-		if err != nil {
-			ch1 <- err.Error()
-			return
-		}
-		ch1 <- fmt.Sprintf("%s\n%v", url, result)
-	}()
+	go brasilAPIRequest(ch1)
 
-	go func() {
-		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-		defer cancel()
-		service := services.NewViaCepZipCodeService()
-		result, url, err := service.GetZipCode(ctx, "01153000")
-		if err != nil {
-			ch2 <- err.Error()
-			return
-		}
-		ch2 <- fmt.Sprintf("%s\n%v", url, result)
-	}()
+	go viaCepRequest(ch2)
 
 	// Receive data from ch2
 	select {
@@ -42,5 +22,44 @@ func main() {
 		fmt.Println(res)
 	case res := <-ch2:
 		fmt.Println(res)
+	}
+}
+
+func brasilAPIRequest(ch1 chan string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	for {
+
+		service := services.NewBrazilApiZipCodeService()
+		result, url, _ := service.GetZipCode(ctx, "01153000")
+		select {
+		case <-ctx.Done():
+			ch1 <- "Request timeout"
+		default:
+			if result != nil {
+				ch1 <- fmt.Sprintf("%s\n%v", url, result)
+				return
+			}
+		}
+	}
+
+}
+
+func viaCepRequest(ch2 chan string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	for {
+
+		service := services.NewViaCepZipCodeService()
+		result, url, _ := service.GetZipCode(ctx, "01153000")
+		select {
+		case <-ctx.Done():
+			ch2 <- "Request timeout"
+		default:
+			if result != nil {
+				ch2 <- fmt.Sprintf("%s\n%v", url, result)
+				return
+			}
+		}
 	}
 }
